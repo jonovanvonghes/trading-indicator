5519a69554588d0ca8f1843e1725da432c15bef5
jonovanvonghes <39641067+jonovanvonghes@users.noreply.github.com>
2021-03-03 09:45:48 -1000

    Update mfi.js
    Added limit

--- indicators/mfi.js
+++ indicators/mfi.js
@@ -2,9 +2,9 @@ const getOHLCV = require('./ohlcv.js')
 const detachSource = require('./source.js')
 const indicators = require('technicalindicators')
 
-const mfi = async (mfiPeriod, ex, ticker, interval, isFuture = false) => {
+const mfi = async (mfiPeriod, ex, ticker, interval, isFuture = false, limit = 500) => {
     try {
-        let ohlcv = await getOHLCV(ex, ticker, interval, isFuture)
+        let ohlcv = await getOHLCV(ex, ticker, interval, isFuture, limit)
         let source = detachSource(ohlcv)
         let mfiInput = {
             high: source['high'],
